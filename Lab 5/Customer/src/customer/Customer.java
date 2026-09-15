644ba52a6311f44b2e02edf151c77595ab702225
Your Name <aryanzsingh444@gmail.com>
2026-09-16 11:45:36 +1200

    Update client to use remote banking operations

--- Lab 5/Customer/src/customer/Customer.java
+++ Lab 5/Customer/src/customer/Customer.java
@@ -4,8 +4,11 @@
  */
 package customer;
 
-import java.rmi.registry.*;
-import api.*;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import api.Api;
+import api.Data;
 
 public class Customer {
     private static final String HOST = "localhost";
@@ -17,9 +20,14 @@ public class Customer {
     public static void main(String[] args) throws Exception {
         registry = LocateRegistry.getRegistry(HOST, PORT);
         Api remoteApi = (Api) registry.lookup(Api.class.getSimpleName());
-        
         System.out.println("New balance = " +
-                remoteApi.setBalance(1000));
+            remoteApi.setBalance(new Data(1000)).getValue());
+        System.out.println("After deposit = " +
+            remoteApi.addBalance(new Data(250)).getValue());
+        System.out.println("After withdrawal = " +
+            remoteApi.withdrawBalance(new Data(100)).getValue());
+        System.out.println("After interest = " +
+            remoteApi.addInterest(new Data(10)).getValue());
         
     }
 }
