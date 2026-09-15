80c53776aa9b0b88eeedc8f2891f7ac2a08f7969
Your Name <aryanzsingh444@gmail.com>
2026-09-16 11:45:32 +1200

    Implement remote banking operations

--- Lab 5/Bank/src/bank/ApiImpl.java
+++ Lab 5/Bank/src/bank/ApiImpl.java
@@ -4,9 +4,11 @@
  */
 package bank;
 
-import java.rmi.*;
-import java.rmi.server.*;
-import api.*;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+import api.Api;
+import api.Data;
 
 public class ApiImpl extends UnicastRemoteObject implements Api {
     private static final long serialVersionUID = 1L;
@@ -14,6 +16,48 @@ public class ApiImpl extends UnicastRemoteObject implements Api {
 
     public ApiImpl() throws RemoteException {
         super();
+    }
+
+    @Override
+    public synchronized int addBalance(int amount) throws RemoteException {
+        return setBalance(account.getValue() + amount);
+    }
+
+    @Override
+    public synchronized int withdrawBalance(int amount) throws RemoteException {
+        return setBalance(account.getValue() - amount);
+    }
+
+    @Override
+    public synchronized double addInterest(double rate) throws RemoteException {
+        double newBalance = account.getValue() * (1 + rate / 100);
+        account.setValue((int) newBalance);
+        System.out.println("new balance: " + account.getValue());
+        return newBalance;
+    }
+
+    @Override
+    public synchronized Data setBalance(Data value) throws RemoteException {
+        setBalance(value.getValue());
+        return new Data(account.getValue());
+    }
+
+    @Override
+    public synchronized Data addBalance(Data amount) throws RemoteException {
+        addBalance(amount.getValue());
+        return new Data(account.getValue());
+    }
+
+    @Override
+    public synchronized Data withdrawBalance(Data amount) throws RemoteException {
+        withdrawBalance(amount.getValue());
+        return new Data(account.getValue());
+    }
+
+    @Override
+    public synchronized Data addInterest(Data rate) throws RemoteException {
+        addInterest(rate.getValue());
+        return new Data(account.getValue());
     }
 
     @Override
