ea5003e05e66ea95c55483087c4bd5e98a283c74
Your Name <aryanzsingh444@gmail.com>
2026-09-16 11:45:27 +1200

    Add balance and interest operations to RMI API

--- Lab 5/Customer/src/api/Api.java
+++ Lab 5/Customer/src/api/Api.java
@@ -9,8 +9,17 @@ package api;
  * @author ultimate7
  */
 
-import java.rmi.*;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface Api extends Remote {
-    public int setBalance(int value)  throws RemoteException;
+    int setBalance(int value) throws RemoteException;
+    int addBalance(int amount) throws RemoteException;
+    int withdrawBalance(int amount) throws RemoteException;
+    double addInterest(double rate) throws RemoteException;
+
+    Data setBalance(Data value) throws RemoteException;
+    Data addBalance(Data amount) throws RemoteException;
+    Data withdrawBalance(Data amount) throws RemoteException;
+    Data addInterest(Data rate) throws RemoteException;
 }
